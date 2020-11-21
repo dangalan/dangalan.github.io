0df9a013e5565dc6738205be2ea906968adf9957
dangalan <alanhuydang@gmail.com>
2020-11-21 01:37:22 -0500

    Fix bug underlining current page nav link on initial load

--- Scripts/onload.js
+++ Scripts/onload.js
@@ -1,5 +1,6 @@
 // Underline current page's nav link
 window.addEventListener("load", function() {
+  console.log(parseFilePathToNavLinkId(window.location.pathname))
   const navLink = document.getElementById(parseFilePathToNavLinkId(window.location.pathname))
   navLink.style.textDecoration = "underline"
   navLink.style.textDecorationColor = "gold"
@@ -7,5 +8,8 @@ window.addEventListener("load", function() {
 });
 
 function parseFilePathToNavLinkId(filePath) {
+  // On initial load the file path is blank
+  if (filePath === "/")
+    return "index-link"
   return filePath.split(".")[0].slice(1).concat("-link")
 }
