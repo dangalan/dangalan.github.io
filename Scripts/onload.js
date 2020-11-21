eafe380bc9a8418191908e054bb683b1b3a8f57f
dangalan <alanhuydang@gmail.com>
2020-11-21 01:58:16 -0500

    Simplified file path parsing

--- Scripts/onload.js
+++ Scripts/onload.js
@@ -7,8 +7,8 @@ window.addEventListener("load", function() {
 });
 
 function parseFilePathToNavLinkId(filePath) {
-  // On homepage file path is blank
+  // Homepage file path is blank
   if (filePath === "/")
     return "home-link"
-  return filePath.split(".")[0].slice(1).concat("-link")
+  return filePath.slice(1).concat("-link")
 }
